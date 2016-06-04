78740d6eadb2e428e80c9da3837f49e95bd515c5
almusto <ales.musto@gmail.com>
2016-06-04 14:04:23 -0400

    Initial Browser Checkpoint Completed

---
+++ Browser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Browser
+//
+//  Created by Alessandro Musto on 6/3/16.
+//  Copyright © 2016 Lmusto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Browser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Browser
+//
+//  Created by Alessandro Musto on 6/3/16.
+//  Copyright © 2016 Lmusto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
